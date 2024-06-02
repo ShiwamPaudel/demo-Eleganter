1568d14229724363622b565a508dcc306f3bde19
ShiwamPaudel <106426216+ShiwamPaudel@users.noreply.github.com>
2024-06-02 12:35:00 +0545

    Added Sections with Icons and Scripts Updated

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');
+    const navlist = document.querySelector('a')
 
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
             header.style.backgroundColor = 'white'; 
-            
+            navlist.style.color="black";
+
         } else {
-            header.style.backgroundColor = 'rgba(51, 51, 51, 0)'; // transpa
+            header.style.backgroundColor = 'rgba(51, 51, 51, 0)'; // transparent
+            navlist.style.color='white';
         }
     });
 
