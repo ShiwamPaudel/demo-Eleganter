1b155ee0151918e5365d9fac7515afa9e7935513
ShiwamPaudel <106426216+ShiwamPaudel@users.noreply.github.com>
2024-06-02 21:54:07 +0545

    Fixed NavBar on Scroll Features and Added Footer

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');
-    const navlist = document.querySelector('a')
+    const navLinks = document.querySelectorAll('nav ul li a');
+    var logoLight = document.getElementById('logo-light');
+    var logoDark = document.getElementById('logo-dark');
 
     window.addEventListener('scroll', function() {
         if (window.scrollY > 50) {
             header.style.backgroundColor = 'white'; 
-            navlist.style.color="black";
+            navLinks.forEach(link => {
+                link.style.color = 'black'; //making it black
+            });
+
+            // LogoChanging on Scroll 
+            logoLight.style.display = 'none';
+            logoDark.style.display = 'block';
 
         } else {
             header.style.backgroundColor = 'rgba(51, 51, 51, 0)'; // transparent
-            navlist.style.color='white';
+            navLinks.forEach(link => {
+                link.style.color = 'white'; 
+            });
+
+            // Logo Reseting to default on not scrolling 
+            logoLight.style.display = 'block';
+            logoDark.style.display = 'none';
+
         }
     });
 
